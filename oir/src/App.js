0660bbb5487852ffa0e8c998ebe1d9a5af741f61
atharvb13 <atharv.22010493@viit.ac.in>
2023-04-05 14:33:41 +0530

    contact and forgotpass added

--- oir/src/App.js
+++ oir/src/App.js
@@ -6,12 +6,17 @@ const MainPage = React.lazy(() => import('./pages/Main/Main'))
 const HomePage = React.lazy(() => import('./pages/Home/Home'))
 const EventsPage = React.lazy(() => import('./pages/Events'))
 const InternshipPage = React.lazy(() => import('./pages/Internship'))
+const LoginPage = React.lazy(() => import('./pages/login'))
+const ContactPage = React.lazy(() => import('./pages/Contact'))
+const ForgotPage = React.lazy(() => import('./pages/ForgotPass'))
+
 
 function App() {
 
   return (
     <>
       <div className="font-Montserrat overflow-x-hidden">
+        
         <BrowserRouter>
           <Suspense>
             <Routes>
@@ -19,7 +24,10 @@ function App() {
               <Route path='/' element={ <MainPage />}>
                 <Route path='' element={ <HomePage />}/> 
                 <Route path='events' element={ <EventsPage />}/> 
+                <Route path='login' element={ <LoginPage />}/> 
                 <Route path='internships' element={ <InternshipPage />}/> 
+                <Route path='contact' element={ <ContactPage />}/> 
+                <Route path='forgot' element={ <ForgotPage />}/> 
               </Route> 
               
             </Routes>
