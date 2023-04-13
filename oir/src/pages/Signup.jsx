e495c62ab18a7d2c3080a4466496a84784f43573
Harsh Upadhye <harshupadhye2832@gmail.com>
2023-04-13 23:01:48 +0530

    Comment added

--- oir/src/pages/Signup.jsx
+++ oir/src/pages/Signup.jsx
@@ -20,10 +20,12 @@ const Signup = () => {
   });
   const navigate = useNavigate();
   const handleChange = (event) => {
+    console.log("HandleChange")
     const newData = { ...data, [event.target.name]: event.target.value };
     setData(newData);
   };
   const validateForm = () => {
+    console.log("Validate")
     let formIsValid = true;
     const newErrors = {
       email: "",
@@ -67,6 +69,7 @@ const Signup = () => {
     return formIsValid;
   };
   const handleSubmit = async () => {
+    console.log("Handle Submit")
     const formIsValid = validateForm();
     if (!formIsValid) {
       return;
@@ -90,86 +93,114 @@ const Signup = () => {
   };
   return (
     <div>
-    <div className="rectangle hidden lg:block">
-    </div>
-    <div className="circle hidden lg:block" >
-    <div className='text'>
-    <h1 className='text-4xl mt-24'>New Here ?</h1>
-    <p className='mt-24 mr-8 text-2xl para text-white'>Sign up and discover  great new opportunities!</p>
-    <div className='flex'>
-    
-    <p className='mt-10 pr-24 text-white'><input type='checkbox' className='mr-4'/>Creating an account means you are okay with our <a className='text-oirOrange'>Terms and of Service</a> and our <a className='text-oirOrange'>Privacy Policy</a></p>
-    </div>
-   <button className='w-32 h-12 mt-8 rounded-full bg-gradient-to-r from-oirYellow to-oirOrange button ml-24'>Hello</button>
-    </div>
-    </div>
-  <section className="h-screen lg:mr-20 pr-8">
-  <div className="h-full lg:mr-10">
-    <div
-      className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
-      <div
-        className="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
+      <div className="rectangle hidden lg:block"></div>
+      <div className="circle hidden lg:block">
+        <div className="text">
+          <h1 className="text-4xl mt-24">New Here ?</h1>
+          <p className="mt-24 mr-8 text-2xl para text-white">
+            Sign up and discover great new opportunities!
+          </p>
+          <div className="flex">
+            <p className="mt-10 pr-24 text-white">
+              <input type="checkbox" className="mr-4" />
+              Creating an account means you are okay with our{" "}
+              <a className="text-oirOrange">Terms and of Service</a> and our{" "}
+              <a className="text-oirOrange">Privacy Policy</a>
+            </p>
+          </div>
+          <button className="w-32 h-12 mt-8 rounded-full bg-gradient-to-r from-oirYellow to-oirOrange button ml-24">
+            Hello
+          </button>
+        </div>
       </div>
-      <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
-        <h2 className='underline decoration-yellow-500 font-bold text-amber-900 text-2xl mb-7'>Sign Up</h2>
-        <form method='post'>
-        <div class="relative mb-3">
-      <input
-        type="text"
-        class="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-white bg-clip-padding py-4 px-3 text-base font-normal leading-tight text-neutral-700 ease-in-out placeholder:text-transparent focus:border-primary focus:bg-white focus:pt-[1.625rem] focus:pb-[0.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:bg-neutral-800 dark:text-neutral-200 [&:not(:placeholder-shown)]:pt-[1.625rem] [&:not(:placeholder-shown)]:pb-[0.625rem]"
-        id="floatingInput"
-        placeholder="Enter First Name" />
-      <label
-        for="floatingInput"
-        class="pointer-events-none absolute top-0 left-0 origin-[0_0] border border-solid border-transparent py-4 px-3 text-neutral-700 transition-[opacity,_transform] duration-100 ease-in-out peer-focus:translate-x-[0.15rem] peer-focus:-translate-y-2 peer-focus:scale-[0.85] peer-focus:opacity-[0.65] peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:scale-[0.85] peer-[:not(:placeholder-shown)]:opacity-[0.65] motion-reduce:transition-none dark:text-neutral-200"
-        >First Name</label>
+      <section className="h-screen lg:mr-20 pr-8">
+        <div className="h-full lg:mr-10">
+          <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
+            <div className="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12"></div>
+            <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
+              <h2 className="underline decoration-yellow-500 font-bold text-amber-900 text-2xl mb-7">
+                Sign Up
+              </h2>
+              <form method="post">
+                <div className="relative mb-3">
+                  <input
+                    type="text"
+                    className="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-white bg-clip-padding py-4 px-3 text-base font-normal leading-tight text-neutral-700 ease-in-out placeholder:text-transparent focus:border-primary focus:bg-white focus:pt-[1.625rem] focus:pb-[0.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:bg-neutral-800 dark:text-neutral-200 [&:not(:placeholder-shown)]:pt-[1.625rem] [&:not(:placeholder-shown)]:pb-[0.625rem]"
+                    id="floatingInput"
+                    onChange={handleChange}
+                    placeholder="Enter First Name"
+                  />
+                  <label
+                    for="floatingInput"
+                    className="pointer-events-none absolute top-0 left-0 origin-[0_0] border border-solid border-transparent py-4 px-3 text-neutral-700 transition-[opacity,_transform] duration-100 ease-in-out peer-focus:translate-x-[0.15rem] peer-focus:-translate-y-2 peer-focus:scale-[0.85] peer-focus:opacity-[0.65] peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:scale-[0.85] peer-[:not(:placeholder-shown)]:opacity-[0.65] motion-reduce:transition-none dark:text-neutral-200"
+                  >
+                    First Name
+                  </label>
+                </div>
+                <div className="relative mb-3">
+                  <input
+                    type="text"
+                    className="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-white bg-clip-padding py-4 px-3 text-base font-normal leading-tight text-neutral-700 ease-in-out placeholder:text-transparent focus:border-primary focus:bg-white focus:pt-[1.625rem] focus:pb-[0.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:bg-neutral-800 dark:text-neutral-200 [&:not(:placeholder-shown)]:pt-[1.625rem] [&:not(:placeholder-shown)]:pb-[0.625rem]"
+                    id="floatingPassword"
+                    onChange={handleChange}
+                    placeholder="Enter last Name"
+                  />
+                  <label
+                    for="floatingPassword"
+                    className="pointer-events-none absolute top-0 left-0 origin-[0_0] border border-solid border-transparent py-4 px-3 text-neutral-700 transition-[opacity,_transform] duration-100 ease-in-out peer-focus:translate-x-[0.15rem] peer-focus:-translate-y-2 peer-focus:scale-[0.85] peer-focus:opacity-[0.65] peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:scale-[0.85] peer-[:not(:placeholder-shown)]:opacity-[0.65] motion-reduce:transition-none dark:text-neutral-200"
+                  >
+                    Last Name
+                  </label>
+                </div>
+                <div className="relative mb-3">
+                  <input
+                    type="email"
+                    className="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-white bg-clip-padding py-4 px-3 text-base font-normal leading-tight text-neutral-700 ease-in-out placeholder:text-transparent focus:border-primary focus:bg-white focus:pt-[1.625rem] focus:pb-[0.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:bg-neutral-800 dark:text-neutral-200 [&:not(:placeholder-shown)]:pt-[1.625rem] [&:not(:placeholder-shown)]:pb-[0.625rem]"
+                    id="floatingInput"
+                    onChange={handleChange}
+                    placeholder="name@example.com"
+                  />
+                  <label
+                    for="floatingInput"
+                    className="pointer-events-none absolute top-0 left-0 origin-[0_0] border border-solid border-transparent py-4 px-3 text-neutral-700 transition-[opacity,_transform] duration-100 ease-in-out peer-focus:translate-x-[0.15rem] peer-focus:-translate-y-2 peer-focus:scale-[0.85] peer-focus:opacity-[0.65] peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:scale-[0.85] peer-[:not(:placeholder-shown)]:opacity-[0.65] motion-reduce:transition-none dark:text-neutral-200"
+                  >
+                    Email address
+                  </label>
+                </div>
+                <div className="relative mb-3">
+                  <input
+                    type="password"
+                    className="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-white bg-clip-padding py-4 px-3 text-base font-normal leading-tight text-neutral-700 ease-in-out placeholder:text-transparent focus:border-primary focus:bg-white focus:pt-[1.625rem] focus:pb-[0.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:bg-neutral-800 dark:text-neutral-200 [&:not(:placeholder-shown)]:pt-[1.625rem] [&:not(:placeholder-shown)]:pb-[0.625rem]"
+                    id="floatingPassword"
+                    onChange={handleChange}
+                    placeholder="Password"
+                  />
+                  <label
+                    for="floatingPassword"
+                    className="pointer-events-none absolute top-0 left-0 origin-[0_0] border border-solid border-transparent py-4 px-3 text-neutral-700 transition-[opacity,_transform] duration-100 ease-in-out peer-focus:translate-x-[0.15rem] peer-focus:-translate-y-2 peer-focus:scale-[0.85] peer-focus:opacity-[0.65] peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:scale-[0.85] peer-[:not(:placeholder-shown)]:opacity-[0.65] motion-reduce:transition-none dark:text-neutral-200"
+                  >
+                    Password
+                  </label>
+                </div>
+                <div className="block lg:hidden mb-4">
+                  <p className="pr-24 text-amber-800">
+                    <input type="checkbox" className="mr-4" />
+                    You agree with our{" "}
+                    <a className="text-oirOrange">Terms of Service</a> and our{" "}
+                    <a className="text-oirOrange">Privacy Policy</a>
+                  </p>
+                </div>
+                <Button
+                  text={"Register"}
+                  onClick={handleSubmit}
+                  className="mt-5"
+                ></Button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-    <div class="relative mb-3">
-      <input
-        type="text"
-        class="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-white bg-clip-padding py-4 px-3 text-base font-normal leading-tight text-neutral-700 ease-in-out placeholder:text-transparent focus:border-primary focus:bg-white focus:pt-[1.625rem] focus:pb-[0.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:bg-neutral-800 dark:text-neutral-200 [&:not(:placeholder-shown)]:pt-[1.625rem] [&:not(:placeholder-shown)]:pb-[0.625rem]"
-        id="floatingPassword"
-        placeholder="Enter last Name" />
-      <label
-        for="floatingPassword"
-        class="pointer-events-none absolute top-0 left-0 origin-[0_0] border border-solid border-transparent py-4 px-3 text-neutral-700 transition-[opacity,_transform] duration-100 ease-in-out peer-focus:translate-x-[0.15rem] peer-focus:-translate-y-2 peer-focus:scale-[0.85] peer-focus:opacity-[0.65] peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:scale-[0.85] peer-[:not(:placeholder-shown)]:opacity-[0.65] motion-reduce:transition-none dark:text-neutral-200"
-        >Last Name</label>
-    </div>
-    <div class="relative mb-3">
-      <input
-        type="email"
-        class="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-white bg-clip-padding py-4 px-3 text-base font-normal leading-tight text-neutral-700 ease-in-out placeholder:text-transparent focus:border-primary focus:bg-white focus:pt-[1.625rem] focus:pb-[0.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:bg-neutral-800 dark:text-neutral-200 [&:not(:placeholder-shown)]:pt-[1.625rem] [&:not(:placeholder-shown)]:pb-[0.625rem]"
-        id="floatingInput"
-        placeholder="name@example.com" />
-      <label
-        for="floatingInput"
-        class="pointer-events-none absolute top-0 left-0 origin-[0_0] border border-solid border-transparent py-4 px-3 text-neutral-700 transition-[opacity,_transform] duration-100 ease-in-out peer-focus:translate-x-[0.15rem] peer-focus:-translate-y-2 peer-focus:scale-[0.85] peer-focus:opacity-[0.65] peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:scale-[0.85] peer-[:not(:placeholder-shown)]:opacity-[0.65] motion-reduce:transition-none dark:text-neutral-200"
-        >Email address</label>
-    </div>
-    <div class="relative mb-3">
-      <input
-        type="password"
-        class="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-white bg-clip-padding py-4 px-3 text-base font-normal leading-tight text-neutral-700 ease-in-out placeholder:text-transparent focus:border-primary focus:bg-white focus:pt-[1.625rem] focus:pb-[0.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:bg-neutral-800 dark:text-neutral-200 [&:not(:placeholder-shown)]:pt-[1.625rem] [&:not(:placeholder-shown)]:pb-[0.625rem]"
-        id="floatingPassword"
-        placeholder="Password" />
-      <label
-        for="floatingPassword"
-        class="pointer-events-none absolute top-0 left-0 origin-[0_0] border border-solid border-transparent py-4 px-3 text-neutral-700 transition-[opacity,_transform] duration-100 ease-in-out peer-focus:translate-x-[0.15rem] peer-focus:-translate-y-2 peer-focus:scale-[0.85] peer-focus:opacity-[0.65] peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:scale-[0.85] peer-[:not(:placeholder-shown)]:opacity-[0.65] motion-reduce:transition-none dark:text-neutral-200"
-        >Password</label>
-    </div>
-
-
-          
-
-         <div className='block lg:hidden mb-4'> 
-         <p className='pr-24 text-amber-800'><input type='checkbox' className='mr-4'/>You agree with our <a className='text-oirOrange'>Terms of Service</a> and our <a className='text-oirOrange'>Privacy Policy</a></p></div>
-          <Button text={"Register"} className="mt-5"></Button>
-        </form>
-      </div>
-    </div>
-  </div>
-</section>
-</div>
   );
 };
 
