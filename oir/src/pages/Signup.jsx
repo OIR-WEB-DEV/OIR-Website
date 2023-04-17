68d065fb58a014d83736db8fe15d62d7328b00cd
Harsh Upadhye <harshupadhye2832@gmail.com>
2023-04-17 11:18:51 +0530

    SignUp integrated

--- oir/src/pages/Signup.jsx
+++ oir/src/pages/Signup.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import Button from "../Components/Button/Button";
+import { useNavigate } from 'react-router-dom';
+// import Button from "../Components/Button/Button";
+import {toast,Toaster} from 'react-hot-toast';
 import "./events.css";
 import axios from "axios";
 
@@ -11,79 +13,100 @@ const Signup = () => {
     password: "",
   });
 
-  const { firstName, lastName, email, password } = data;
+  const navigate = useNavigate();
 
-  const [error, setError] = useState({
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [errors, setErrors] = useState({
     firstName: "",
     lastName: "",
     email: "",
     password: "",
   });
 
-  const handleChange = (event) => {
-    console.log("Handle Change");
-    const newData = { ...data, [event.target.name]: event.target.value };
-    setdata(newData);
-  };
-
   const validateForm = () => {
-    console.log("Validate Form");
     let formIsValid = true;
     const newErrors = {
-      email: "",
-      password: "",
-      confirmpassword: "",
       firstName: "",
       lastName: "",
-      phoneNumber: "",
+      email: "",
+      password: "",
     };
 
     if (!email) {
       newErrors.email = "Email is required";
+      toast.error(newErrors.email);
       formIsValid = false;
+      
     } else if (!/\S+@\S+\.\S+/.test(email)) {
       newErrors.email = "Email is invalid";
+      toast.error(newErrors.email)
       formIsValid = false;
     }
 
     if (!password) {
       newErrors.password = "Password is required";
+      toast.error(newErrors.password)
       formIsValid = false;
-    } else if (password.length < 8) {
-      newErrors.password = "Password should be at least 8 characters long";
+    } else if (password.length < 6) {
+      newErrors.password = "Password should be at least 6 characters long";
+      toast.error(newErrors.password)
       formIsValid = false;
     }
 
     if (!firstName) {
       newErrors.firstName = "First Name is required";
+      toast.error(newErrors.firstName)
       formIsValid = false;
     }
 
     if (!lastName) {
       newErrors.lastName = "Last Name is required";
+      toast.error(newErrors.lastName)
       formIsValid = false;
     }
-
-    setError(newErrors);
+    setErrors(newErrors);
     return formIsValid;
   };
 
-  const handleSubmit = async () => {
-    console.log("Handle Submit");
+  const { firstName, lastName, email, password } = data;
+
+  const handleChange = (event) => {
+    const newData = { ...data, [event.target.name]: event.target.value };
+    setdata(newData);
+  };
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    if (isLoading) return;
     const formIsValid = validateForm();
     if (!formIsValid) {
       return;
     }
-    try {
+    setIsLoading(true);
+    try{
       const result = await axios.post(
-        "https://oir-server.vercel.app/api/v1/register",
+        "http://localhost:4000/api/v1/register",
         data
-      );
-      console.log(result);
-    } catch (e) {
-      console.error(`Error signing up: ${e}`);
+      )
+      console.log(result)
+      if(result.data.success)
+      {
+        toast.success(result.data.message,{duration:5000});
+        setIsLoading(false);
+        navigate('/')
+      }
+      else{
+        toast.error("Failed to Register");
+      }
+      setIsLoading(false);
+    } catch(error){
+      toast.error(error.response.data.message,{duration:5000})
+      setIsLoading(false);
     }
-  };
+
+  }
+
   return (
     <div>
       <div className="rectangle hidden lg:block"></div>
@@ -114,67 +137,71 @@ const Signup = () => {
               <h2 className="underline decoration-yellow-500 font-bold text-amber-900 text-2xl mb-7">
                 Sign Up
               </h2>
-              <form method="post">
-                <div class="relative mb-3">
+              <form method="post" >
+                <div className="relative mb-3">
                   <input
                     type="text"
-                    class="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-white bg-clip-padding py-4 px-3 text-base font-normal leading-tight text-neutral-700 ease-in-out placeholder:text-transparent focus:border-primary focus:bg-white focus:pt-[1.625rem] focus:pb-[0.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:bg-neutral-800 dark:text-neutral-200 [&:not(:placeholder-shown)]:pt-[1.625rem] [&:not(:placeholder-shown)]:pb-[0.625rem]"
-                    placeholder="Enter First Name"
+                    className="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-white bg-clip-padding py-4 px-3 text-base font-normal leading-tight text-neutral-700 ease-in-out placeholder:text-transparent focus:border-primary focus:bg-white focus:pt-[1.625rem] focus:pb-[0.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:bg-neutral-800 dark:text-neutral-200 [&:not(:placeholder-shown)]:pt-[1.625rem] [&:not(:placeholder-shown)]:pb-[0.625rem]"
                     id="firstName"
+                    name="firstName"
+                    placeholder="Enter First Name"
+                    value={data.firstName}
                     onChange={handleChange}
                     required
                   />
                   <label
-                    for="floatingInput"
-                    class="pointer-events-none absolute top-0 left-0 origin-[0_0] border border-solid border-transparent py-4 px-3 text-neutral-700 transition-[opacity,_transform] duration-100 ease-in-out peer-focus:translate-x-[0.15rem] peer-focus:-translate-y-2 peer-focus:scale-[0.85] peer-focus:opacity-[0.65] peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:scale-[0.85] peer-[:not(:placeholder-shown)]:opacity-[0.65] motion-reduce:transition-none dark:text-neutral-200"
+                    className="pointer-events-none absolute top-0 left-0 origin-[0_0] border border-solid border-transparent py-4 px-3 text-neutral-700 transition-[opacity,_transform] duration-100 ease-in-out peer-focus:translate-x-[0.15rem] peer-focus:-translate-y-2 peer-focus:scale-[0.85] peer-focus:opacity-[0.65] peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:scale-[0.85] peer-[:not(:placeholder-shown)]:opacity-[0.65] motion-reduce:transition-none dark:text-neutral-200"
                   >
                     First Name
                   </label>
                 </div>
-                <div class="relative mb-3">
+                <div className="relative mb-3">
                   <input
                     type="text"
-                    class="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-white bg-clip-padding py-4 px-3 text-base font-normal leading-tight text-neutral-700 ease-in-out placeholder:text-transparent focus:border-primary focus:bg-white focus:pt-[1.625rem] focus:pb-[0.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:bg-neutral-800 dark:text-neutral-200 [&:not(:placeholder-shown)]:pt-[1.625rem] [&:not(:placeholder-shown)]:pb-[0.625rem]"
-                    placeholder="Enter last Name"
+                    className="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-white bg-clip-padding py-4 px-3 text-base font-normal leading-tight text-neutral-700 ease-in-out placeholder:text-transparent focus:border-primary focus:bg-white focus:pt-[1.625rem] focus:pb-[0.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:bg-neutral-800 dark:text-neutral-200 [&:not(:placeholder-shown)]:pt-[1.625rem] [&:not(:placeholder-shown)]:pb-[0.625rem]"
                     id="lastName"
+                    name="lastName"
+                    placeholder="Enter Last Name"
+                    value={data.lastName}
                     onChange={handleChange}
                     required
                   />
                   <label
-                    for="floatingPassword"
-                    class="pointer-events-none absolute top-0 left-0 origin-[0_0] border border-solid border-transparent py-4 px-3 text-neutral-700 transition-[opacity,_transform] duration-100 ease-in-out peer-focus:translate-x-[0.15rem] peer-focus:-translate-y-2 peer-focus:scale-[0.85] peer-focus:opacity-[0.65] peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:scale-[0.85] peer-[:not(:placeholder-shown)]:opacity-[0.65] motion-reduce:transition-none dark:text-neutral-200"
+                    className="pointer-events-none absolute top-0 left-0 origin-[0_0] border border-solid border-transparent py-4 px-3 text-neutral-700 transition-[opacity,_transform] duration-100 ease-in-out peer-focus:translate-x-[0.15rem] peer-focus:-translate-y-2 peer-focus:scale-[0.85] peer-focus:opacity-[0.65] peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:scale-[0.85] peer-[:not(:placeholder-shown)]:opacity-[0.65] motion-reduce:transition-none dark:text-neutral-200"
                   >
                     Last Name
                   </label>
                 </div>
-                <div class="relative mb-3">
+                <div className="relative mb-3">
                   <input
                     type="email"
-                    class="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-white bg-clip-padding py-4 px-3 text-base font-normal leading-tight text-neutral-700 ease-in-out placeholder:text-transparent focus:border-primary focus:bg-white focus:pt-[1.625rem] focus:pb-[0.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:bg-neutral-800 dark:text-neutral-200 [&:not(:placeholder-shown)]:pt-[1.625rem] [&:not(:placeholder-shown)]:pb-[0.625rem]"
-                    placeholder="name@example.com"
+                    className="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-white bg-clip-padding py-4 px-3 text-base font-normal leading-tight text-neutral-700 ease-in-out placeholder:text-transparent focus:border-primary focus:bg-white focus:pt-[1.625rem] focus:pb-[0.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:bg-neutral-800 dark:text-neutral-200 [&:not(:placeholder-shown)]:pt-[1.625rem] [&:not(:placeholder-shown)]:pb-[0.625rem]"
+                    name="email"
                     id="email"
+                    placeholder="Enter Email"
+                    value={data.email}
                     onChange={handleChange}
                     required
                   />
                   <label
-                    for="floatingInput"
-                    class="pointer-events-none absolute top-0 left-0 origin-[0_0] border border-solid border-transparent py-4 px-3 text-neutral-700 transition-[opacity,_transform] duration-100 ease-in-out peer-focus:translate-x-[0.15rem] peer-focus:-translate-y-2 peer-focus:scale-[0.85] peer-focus:opacity-[0.65] peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:scale-[0.85] peer-[:not(:placeholder-shown)]:opacity-[0.65] motion-reduce:transition-none dark:text-neutral-200"
+                    className="pointer-events-none absolute top-0 left-0 origin-[0_0] border border-solid border-transparent py-4 px-3 text-neutral-700 transition-[opacity,_transform] duration-100 ease-in-out peer-focus:translate-x-[0.15rem] peer-focus:-translate-y-2 peer-focus:scale-[0.85] peer-focus:opacity-[0.65] peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:scale-[0.85] peer-[:not(:placeholder-shown)]:opacity-[0.65] motion-reduce:transition-none dark:text-neutral-200"
                   >
                     Email address
                   </label>
                 </div>
-                <div class="relative mb-3">
+                <div className="relative mb-3">
                   <input
                     type="password"
-                    class="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-white bg-clip-padding py-4 px-3 text-base font-normal leading-tight text-neutral-700 ease-in-out placeholder:text-transparent focus:border-primary focus:bg-white focus:pt-[1.625rem] focus:pb-[0.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:bg-neutral-800 dark:text-neutral-200 [&:not(:placeholder-shown)]:pt-[1.625rem] [&:not(:placeholder-shown)]:pb-[0.625rem]"
-                    placeholder="Password"
+                    className="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-white bg-clip-padding py-4 px-3 text-base font-normal leading-tight text-neutral-700 ease-in-out placeholder:text-transparent focus:border-primary focus:bg-white focus:pt-[1.625rem] focus:pb-[0.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:bg-neutral-800 dark:text-neutral-200 [&:not(:placeholder-shown)]:pt-[1.625rem] [&:not(:placeholder-shown)]:pb-[0.625rem]"
                     id="password"
+                    name="password"
+                    placeholder="Enter Password"
+                    value={data.password}
                     onChange={handleChange}
                     required
                   />
                   <label
-                    for="floatingPassword"
-                    class="pointer-events-none absolute top-0 left-0 origin-[0_0] border border-solid border-transparent py-4 px-3 text-neutral-700 transition-[opacity,_transform] duration-100 ease-in-out peer-focus:translate-x-[0.15rem] peer-focus:-translate-y-2 peer-focus:scale-[0.85] peer-focus:opacity-[0.65] peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:scale-[0.85] peer-[:not(:placeholder-shown)]:opacity-[0.65] motion-reduce:transition-none dark:text-neutral-200"
+                    className="pointer-events-none absolute top-0 left-0 origin-[0_0] border border-solid border-transparent py-4 px-3 text-neutral-700 transition-[opacity,_transform] duration-100 ease-in-out peer-focus:translate-x-[0.15rem] peer-focus:-translate-y-2 peer-focus:scale-[0.85] peer-focus:opacity-[0.65] peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:scale-[0.85] peer-[:not(:placeholder-shown)]:opacity-[0.65] motion-reduce:transition-none dark:text-neutral-200"
                   >
                     Password
                   </label>
@@ -190,12 +217,16 @@ const Signup = () => {
                 </div>
                 {/* <Button text={"Register"} onSubmit={handleSubmit}  className="mt-5"></Button> */}
                 <div className=" mt-5 cursor-pointer flex items-center py-3 px-8 rounded-full border-black text-white justify-center bg-gradient-to-r from-oirYellow to-oirOrange transition duration-100 ease-in-out">
-                  <button onSubmit={handleSubmit} className=" text-white font-bold">Register</button>
+                  <button type="submit" onClick={handleSubmit} className=" text-white font-bold">Register</button>
                 </div>
               </form>
             </div>
           </div>
         </div>
+        <Toaster
+          position="top-center"
+          reverseOrder={true}
+        />
       </section>
     </div>
   );
