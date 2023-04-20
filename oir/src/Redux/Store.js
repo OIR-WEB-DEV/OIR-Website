ec391ba5b50d9afbac203b6ae725b417494a8c15
Harsh Upadhye <harshupadhye2832@gmail.com>
2023-04-20 13:18:46 +0530

    update

--- oir/src/Redux/Store.js
+++ oir/src/Redux/Store.js
@@ -4,7 +4,7 @@ import {combineReducers, createStore} from "redux"
 const rootReducer = combineReducers({
     AuthLogin: AuthReducers,
 })
-const store = createStore(
-    rootReducer
-)
+const store = createStore(rootReducer);
+
+
 export default store;
