8f086afe34b8b55de82eceb45019fc3f60d1b4c9
Harsh Upadhye <harshupadhye2832@gmail.com>
2023-04-20 13:18:15 +0530

    OTP added

---
+++ oir/src/pages/OtpPage.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import Button from "../Components/Button/Button";
+import "./events.css";
+import lock from "../assets/lock.png";
+import forgot from "../assets/forgotpass.png";
+import { toast,Toaster } from "react-hot-toast";
+import axios from "axios";
+
+const OtpPage = () => {
+    const handleOTPSubmit = async(e) => {
+        e.preventDefault()
+        try {
+            const result = await axios.post(
+              `https://oir-server.vercel.app/api/v1/verifyUser/`,
+
+            )
+        } catch (error) {
+            
+        }
+    }
+  return (
+    <div>
+      <div className="rectangle invisible lg:visible min-[800px]:invisible"></div>
+      <div className="circle invisible md:visible">
+        <div className="text mt-40 pl-24 ml-[30px]">
+          <div className="rounded-full w-48 h-48 ring-2 ring-amber-500">
+            <img className="ml-6 pt-2 h-36 w-36" src={lock}></img>
+          </div>
+        </div>
+      </div>
+      <section className="h-screen lg:mr-20 pr-8">
+        <div className="h-full lg:mr-10">
+          <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
+            <div className="shrink-1 mb-12 grow-0 invisible basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
+              <img src="" className="w-full" alt="Sample image" />
+            </div>
+            <div className="mb-12 max-[420px]:ml-10 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12 md:mx-auto">
+              <h1 className="font-bold text-amber-900 text-xl mb-7">
+                Verify your email 
+              </h1>
+
+              <form action="" method="post">
+                <h2 className="">OTP</h2>
+
+                <div className="flex flex-row items-center justify-between mr-4 mt-6 w-full max-w-xs">
+                  <div className="w-14 h-14 ">
+                    <input
+                      className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border-2 border-amber-900 text-lg bg-white focus:bg-gray-50 focus:ring-1 focus:ring-orange-400 focus:border-orange-400"
+                      type="text"
+                      maxLength={1}
+                    />
+                  </div>
+                  <div className="w-14 h-14 ">
+                    <input
+                      className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border-2 border-amber-900 text-lg bg-white focus:bg-gray-50 focus:ring-1 focus:ring-orange-500 focus:border-orange-500"
+                      type="text"
+                      maxLength={1}
+                    />
+                  </div>
+                  <div className="w-14 h-14 ">
+                    <input
+                      className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border-2 border-amber-900 text-lg bg-white focus:bg-gray-50 focus:ring-1 focus:ring-orange-500 focus:border-orange-500"
+                      type="text"
+                      maxLength={1}
+                    />
+                  </div>
+                  <div className="w-14 h-14 ">
+                    <input
+                      className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border-2 border-amber-900 text-lg bg-white focus:bg-gray-50 focus:ring-1 focus:ring-orange-500 focus:border-orange-500"
+                      type="text"
+                      maxLength={1}
+                    />
+                  </div>
+                </div>
+                <h2 className="mt-4">Enter the OTP sent on your email</h2>
+
+                <div className="flex flex-col  mr-4">
+                  <div className="md:ml-[0.5rem] mt-4">
+                    <button className="flex flex-row items-center justify-center text-center w-40 border rounded-xl outline-none py-3 cursor-pointer flex items-center py-1 px-4 rounded-full border-black text-white justify-center bg-gradient-to-r from-oirYellow to-oirOrange transition duration-100 ease-in-out  border-none text-white text-md hover:ring-2 hover:ring-blue-600 shadow-sm mt-4">
+                      Submit
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <Toaster
+          position="top-center"
+          reverseOrder={true}
+          
+        />
+      </section>
+    </div>
+  );
+};
+
+export default OtpPage;
