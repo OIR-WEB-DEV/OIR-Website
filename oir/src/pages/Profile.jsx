35db838204980f1368879d116c2525657bc1bb07
Harsh Upadhye <harshupadhye2832@gmail.com>
2023-04-22 17:37:05 +0530

    Profile Update

--- oir/src/pages/Profile.jsx
+++ oir/src/pages/Profile.jsx
@@ -24,7 +24,7 @@ const profile = () => {
             <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
               <h2 className='underline decoration-yellow-500 font-bold text-amber-900 text-2xl mb-7'>Profile</h2>
               <form method='post'>
-                <div className="relative mb-3">
+                <div className="relative mb-3"> 
                   <input
                     type="text"
                     className="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-white bg-clip-padding py-4 px-3 text-base font-normal leading-tight text-neutral-700 ease-in-out placeholder:text-transparent focus:border-primary focus:bg-white focus:pt-[1.625rem] focus:pb-[0.625rem] focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:bg-neutral-800 dark:text-neutral-200 [&:not(:placeholder-shown)]:pt-[1.625rem] [&:not(:placeholder-shown)]:pb-[0.625rem]"
