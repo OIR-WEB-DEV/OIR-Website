fcd1272878a0310d390e45ca7b7818b15632421d
Rohit Nandagawali <nandagawalirohit143@gmail.com>
2023-06-09 02:42:34 +0530

    event page updated

--- oir/src/Components/Heading/Heading.jsx
+++ oir/src/Components/Heading/Heading.jsx
@@ -1,6 +1,6 @@
 const Heading = ({ text }) => {
     return (
-        <h1 className=" m-1 font-bold text-4xl relative text-oirBrown after:content-[''] after:absolute after:w-24 after:-bottom-2 after:left-0 after:h-[2px] after:bg-oirYellow">
+        <h1 className=" m-1 font-bold text-4xl relative text-oirBrown after:content-[''] after:absolute after:w-24 after:-bottom-2 after:left-0 after:h-[2px] after:bg-oirYellow font-Montserrat ">
             {text}
         </h1>
     );
