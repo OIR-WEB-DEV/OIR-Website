9ef2dac0d34e56d81ba2b8c55a39b4f55b20855f
Rohit Nandagawali <nandagawalirohit143@gmail.com>
2023-06-12 17:20:00 +0530

    added spinner
    added spinner before page loading

--- oir/src/pages/Internship.jsx
+++ oir/src/pages/Internship.jsx
@@ -4,7 +4,8 @@ import { intern } from './data'
 import Button from '../Components/Button/Button'
 import Heading from '../Components/Heading/Heading'
 
-const InternshipCards = ({pdf,name,image,text}) => {
+const InternshipCards = ({ pdf, name, image, text }) => {
+   
     return (
 
         <div className=" border border-oirBrown h-[32rem] rounded-sm shadow relative hover:border-oirOrange ease-in duration-150 hover:shadow-2xl cursor-pointer hover:shadow-orange-300">
@@ -38,7 +39,8 @@ const Internship = () => {
                 {intern.map((item) => {
                     const { image, name, text, id, pdf } = item;
                     return (
-                        <InternshipCards key={id} image={image} name={name} text={text} pdf={pdf} />
+                       <InternshipCards key={id} image={image} name={name} text={text} pdf={pdf} />
+                        
                     )
                 })}
 
